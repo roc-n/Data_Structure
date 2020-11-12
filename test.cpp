b00ca3f7dd67fb5bb20e1836bb554c9bfc464f95
roc-n <yugi.t@qq.com>
2020-11-12 22:09:23 +0800

    大二上期中考前夜

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -7,7 +8,7 @@ typedef struct tests
     int b;
     string name;
 } Tests;
-using namespace std;
+
 Tests test()
 {
     Tests test;
@@ -23,5 +24,38 @@ int main(void)
     Tests b = test();
     cout << b.a << "  " << b.b << "  "
          << b.name << endl;
+
+    string a = "Helloszz!";
+    const char *str = a.c_str();
+    cout << str << sizeof(str) << endl;
+    char ss[] = "Hello!";
+    cout << sizeof(ss) << endl;
+    cout << strlen(ss) << endl;
+
+    cout << a[0] << endl;
+    string::iterator i = a.begin();
+    // char *= a.begin();
+    for (i = a.begin(); i != a.end(); ++i)
+    {
+        cout << *i;
+    }
+
+    string q;
+    q += 's';
+    cout << q << endl;
+
+    char ch1 = 'e';
+    string s1;
+    s1.push_back(ch1);
+    cout << a.length() << a.substr(1, 5) << endl;
+    char s = a[0];
+    cout << s << endl;
+
+    // ch可以和string对象直接串接
+    string test = "as";
+    char ch = 'a';
+    test = test + ch;
+    cout << test << endl;
     return 0;
+    // **************************
 }
